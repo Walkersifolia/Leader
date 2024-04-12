964a47bfa0697da81ea12469d03c13946721378f
WalkerTian <903895182@qq.com>
2024-04-12 16:34:04 +0800

    early test

--- leader/__init__.py
+++ leader/__init__.py
@@ -1,8 +1,22 @@
 from mcdreforged.api.all import *
 
-from leader import my_lib
+PLUGIN_METADATA = {
+    'id': 'leader',
+    'version': '1.0.0',
+    'name': 'Leader-Plugin',
+    'description': 'give a player marker',
+    'author': 'WalkerTian',
+    'link': 'https://github.com/Walkersifolia/Leader'
+}
 
+def on_user_info(server: ServerInterface, info: Info):
+    if info.is_player:
+        if info.content == '!!leader':
+            server.execute('effect give {} minecraft:glowing 1000000'.format(info.player))
+        elif info.content == '!!unleader':
+            server.execute('effect clear {} minecraft:glowing'.format(info.player))
 
-def on_load(server: PluginServerInterface, old):
-    server.logger.info(server.tr('my_plugin.a_message'))
-    my_lib.do_something()
+def on_load(server: ServerInterface, old_module):
+    server.register_help_message('!!leader', '高亮自己')
+    server.register_help_message('!!unleader', '取消高亮')
+    server.register_event_listener('minecraft.console.info', on_user_info)
